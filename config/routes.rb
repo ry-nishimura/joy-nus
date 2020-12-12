df2480e73a8ca2ba537c592a25f6b9c065b31517
ry-nishimura <72368098+ry-nishimura@users.noreply.github.com>
2020-12-12 16:48:16 +0900

    ルーティングを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "recruitments#index"
-  resources :recruitments, only: :new, :create
+  resources :recruitments, only: [:new, :create]
 end
