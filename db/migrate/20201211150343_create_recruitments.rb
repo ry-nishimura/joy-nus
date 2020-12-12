f497ff748ab3bc605a3cb6698a134c53ce5308c7
ry-nishimura <72368098+ry-nishimura@users.noreply.github.com>
2020-12-12 16:09:25 +0900

    recruitmentsテーブルを作成

--- db/migrate/20201211150343_create_recruitments.rb
+++ db/migrate/20201211150343_create_recruitments.rb
@@ -1,7 +1,14 @@
 class CreateRecruitments < ActiveRecord::Migration[6.0]
   def change
     create_table :recruitments do |t|
-
+      t.date :date,            null: false
+      t.string :time,          null: false
+      t.string :team,          null: false
+      t.string :sport,         null: false
+      t.string :place,         null: false
+      t.string :position
+      t.integer :experience_id
+      t.references :user,      foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_09_033935) do
+ActiveRecord::Schema.define(version: 2020_12_11_150343) do
+
+  create_table "recruitments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.date "date", null: false
+    t.string "time", null: false
+    t.string "team", null: false
+    t.string "sport", null: false
+    t.string "place", null: false
+    t.string "position"
+    t.integer "experience_id"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_recruitments_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -28,4 +42,5 @@ ActiveRecord::Schema.define(version: 2020_12_09_033935) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "recruitments", "users"
 end
