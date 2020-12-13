dfef45a1b59b7d4f19ab57aabcb60b7c7b980bda
ry-nishimura <72368098+ry-nishimura@users.noreply.github.com>
2020-12-13 23:53:56 +0900

    recruitmentsコントローラーのcrateアクションにデータ保存のための記述を追加

--- app/controllers/recruitments_controller.rb
+++ app/controllers/recruitments_controller.rb
@@ -7,5 +7,11 @@ class RecruitmentsController < ApplicationController
   end
 
   def create
+    @recruitment = Recruitment.create(recruitment_params)
+  end
+
+  private
+  def recruitment_params
+    params.require(:recruitment).permit(:date, :time, :team, :sport, :place, :position, :experience_id).merge(user_id: current_user.id)
   end
 end
